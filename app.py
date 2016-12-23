d2bf3611f05cf5ab214c3e6a28488dd009c4cee8
Alexander Pozharskii <gaussmake1994@yandex.ru>
2016-12-23 15:41:24 +0300

    Added webhook message

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ app = Flask(__name__)
 # noinspection PyPackageRequirements
 @app.route('/webhook', methods=['POST'])
 def webhook():
+    print("Webhook")
     req = request.get_json(silent=True, force=True)
     print("Request:")
     print(json.dumps(req, indent=4))
