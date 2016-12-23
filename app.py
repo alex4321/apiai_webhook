4953999dc1bcbfcdb18add9dfad5999ffe89f048
Alexander Pozharskii <gaussmake1994@yandex.ru>
2016-12-23 16:53:59 +0300

    Added debug information

--- app.py
+++ app.py
@@ -8,8 +8,6 @@ from apiai_webhook import Application, WebHookAnswer, WebHookRequest
 
 def process_request(req):
     print("ACTION : ", req.result.action)
-    if req.result.action != "yahooWeatherForecast":
-        return WebHookAnswer()
     baseurl = "https://query.yahooapis.com/v1/public/yql?"
     yql_query = make_yql_query(req)
     if yql_query is None:
@@ -30,24 +28,32 @@ def make_yql_query(req):
 
 def make_webhook_result(data):
     query = data.get('query')
+    print("QUERY : ", json.dumps(query))
     if query is None:
         return WebHookAnswer()
     result = query.get('results')
+    print("RESULTS : ", json.dumps(result))
     if result is None:
         return WebHookAnswer()
     channel = result.get('channel')
+    print("CHANNEL : ", json.dumps(channel))
     if channel is None:
         return WebHookAnswer()
     item = channel.get('item')
+    print("ITEM : ", json.dumps(item))
     location = channel.get('location')
+    print("LOCATION : ", json.dumps(location))
     units = channel.get('units')
+    print("UNITS : ", json.dumps(units))
     if (location is None) or (item is None) or (units is None):
         return {}
     condition = item.get('condition')
+    print("CONDITION : ", json.dumps(condition))
     if condition is None:
         return {}
     speech = "Today in " + location.get('city') + ": " + condition.get('text') + \
              ", the temperature is " + condition.get('temp') + " " + units.get('temperature')
+    print("SPEECH : ", json.dumps(speech))
     print("Response:")
     print(speech)
     return WebHookAnswer(
