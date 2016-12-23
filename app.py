03e440cb0a3443a07ded6853a1c3ed1a3751ec61
Alexander Pozharskii <gaussmake1994@yandex.ru>
2016-12-23 17:40:02 +0300

    Style

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
-
 import os
 import pyowm
-import json
 from apiai_webhook import Application, WebHookAnswer, WebHookRequest
 
 
@@ -62,50 +60,14 @@ def conditions_view(req):
     if not conditions:
         return WebHookAnswer()
     print("CONDITIONS: ", city, conditions)
-    speech = json.dumps(conditions, ensure_ascii=False)
+    speech = conditions["status"]
     return WebHookAnswer(speech=speech, display_text=speech)
-
-
-#def make_webhook_result(data):
-#    query = data.get('query')
-#    print("QUERY : ", json.dumps(query))
-#    if query is None:
-#        return WebHookAnswer()
-#    result = query.get('results')
-#    print("RESULTS : ", json.dumps(result))
-#    if result is None:
-#        return WebHookAnswer()
-#    channel = result.get('channel')
-#    print("CHANNEL : ", json.dumps(channel))
-#    if channel is None:
-#        return WebHookAnswer()
-#    item = channel.get('item')
-#    print("ITEM : ", json.dumps(item))
-#    location = channel.get('location')
-#    print("LOCATION : ", json.dumps(location))
-#    units = channel.get('units')
-#    print("UNITS : ", json.dumps(units))
-#    if (location is None) or (item is None) or (units is None):
-#        return {}
-#    condition = item.get('condition')
-#    print("CONDITION : ", json.dumps(condition))
-#    if condition is None:
-#        return {}
-#    speech = "Today in " + location.get('city') + ": " + condition.get('text') + \
-#             ", the temperature is " + condition.get('temp') + " " + units.get('temperature')
-#    print("SPEECH : ", json.dumps(speech))
-#    print("Response:")
-#    print(speech)
-#    return WebHookAnswer(
-#        speech=speech,
-#        display_text=speech,
-#        source="apiai-weather-webhook-sample"
-#    )
 
 
 if __name__ == '__main__':
     application = Application("/webhook", {
-        "condition": [conditions_view]
+        "temperature": [temperature_view],
+        "conditions": [conditions_view],
     })
     port = int(os.getenv('PORT', 5000))
     print("Starting app on port {0}".format(port))
